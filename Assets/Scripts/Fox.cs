898fe5b54743ac4bffe9c8ac115a5207a98f0476
SPQR-9 <66369459+SPQR-9@users.noreply.github.com>
2020-10-13 12:40:28 +0300

    init

---
+++ Assets/Scripts/Fox.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Fox : MonoBehaviour
+{
+    private Animator _animator = new Animator();
+    private Rigidbody2D _rigidbody = new Rigidbody2D();
+    private SpriteRenderer _renderer = new SpriteRenderer();
+    private bool _isDetected = false;
+    private bool _isItEscaping = false;
+    [SerializeField] float _speed  = 0f;
+
+    private void Awake()
+    {
+        _animator = gameObject.GetComponent<Animator>();
+        _rigidbody = gameObject.GetComponent<Rigidbody2D>();
+        _renderer = gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    void Start()
+    {
+        _animator.SetBool("Idle", true);
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        if (!_isDetected)
+        {
+            _rigidbody.velocity = transform.right * _speed;
+        }
+        else if (!_isItEscaping)
+        {
+            _animator.SetBool("Run", true);
+            _isItEscaping = true;
+        }
+        if (_animator.GetCurrentAnimatorStateInfo(0).IsName("Run"))
+        {
+            _renderer.flipX = true;
+            _rigidbody.velocity = transform.right * _speed * -2;
+        }
+    }
+
+    public void Detected()
+    {
+        _isDetected = true;
+        _animator.SetBool("Idle", false);
+    }
+}
---
+++ Assets/Scripts/Trigger.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Trigger : MonoBehaviour
+{
+    [SerializeField] private float _timeToDetection = 0f;
+    private AudioSource _source = new AudioSource();
+    private bool _isEnemyExit = false;
+    private Coroutine _signalCoroutine;
+
+    private void Awake()
+    {
+        _source = gameObject.GetComponent<AudioSource>();
+    }
+    private void Update()
+    {
+        if(_source.volume==0  && _isEnemyExit)
+        {
+            StopCoroutine(_signalCoroutine);
+            //StopCoroutine(Signaling());
+            //StopAllCoroutines();
+            _source.Stop();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent<Fox>(out Fox fox))
+        {
+            StartCoroutine(DetectingAnObject(_timeToDetection, fox));
+            _signalCoroutine = StartCoroutine(Signaling());
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.TryGetComponent<Fox>(out Fox fox))
+        {
+            _isEnemyExit = true;
+        }
+    }
+
+    private IEnumerator DetectingAnObject(float value, Fox fox)
+    {
+        yield return new WaitForSeconds(value);
+        fox.Detected();
+    }
+
+    private IEnumerator Signaling()
+    {
+        _source.Play();
+        while (true)
+        {
+            for (float i = 0; i < 1; i+=0.01f)
+            {
+                _source.volume = i;
+                yield return null;
+            }
+            for (float i = 1; i > 0; i-=0.01f)
+            {
+                _source.volume = i;
+                yield return null;
+            }
+        }
+    }
+}
