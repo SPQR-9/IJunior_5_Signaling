07d3e64f7a0822dd3c84d4aec3b9c4bb46a9f457
SPQR-9 <66369459+SPQR-9@users.noreply.github.com>
2020-10-14 23:14:43 +0300

    init1

---
+++ Assets/Scripts/Fox.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(SpriteRenderer))]
+public class Fox : MonoBehaviour
+{
+    [SerializeField] private float _speed = 0f;
+    [SerializeField] private float _responseTime = 0f;
+
+    private Animator _animator;
+    private Rigidbody2D _rigidbody;
+    private SpriteRenderer _renderer;
+    private bool _isDetected = false;
+    private bool _isItEscaping = false;
+    
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _renderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Start()
+    {
+        _animator.SetBool("Idle", true);
+    }
+
+    private void FixedUpdate()
+    {
+        if (!_isDetected)
+        {
+            _rigidbody.velocity = transform.right * _speed;
+        }
+        else if(_isItEscaping)
+        {
+            _rigidbody.velocity = transform.right * _speed * -2;
+        }
+    }
+
+    public void Detected()
+    {
+        _isDetected = true;
+        _animator.SetBool("Idle", false);
+        StartCoroutine(RespondToDanger());
+    }
+
+    public IEnumerator RespondToDanger()
+    {
+        yield return new WaitForSeconds(_responseTime);
+        _animator.SetBool("Run", true);
+        _renderer.flipX = true;
+        _isItEscaping = true;
+    }
+}
---
+++ Assets/Scripts/Signaling.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+
+[RequireComponent(typeof(AudioSource))]
+public class Signaling : MonoBehaviour
+{
+    [SerializeField] private float _timeToDetection = 0f;
+
+    private AudioSource _source;
+    private bool _isEnemyExit = false;
+    private Coroutine _signalCoroutine;
+
+    private void Awake()
+    {
+        _source = GetComponent<AudioSource>();
+    }
+    private void Update()
+    {
+        if(_source.volume==0  && _isEnemyExit)
+        {
+            StopCoroutine(_signalCoroutine);
+            _source.Stop();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent<Fox>(out Fox fox))
+        {
+            StartCoroutine(DetectingAnObject(_timeToDetection, fox));
+            _signalCoroutine = StartCoroutine(AlarmOperation());
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.TryGetComponent<Fox>(out Fox fox))
+        {
+            _isEnemyExit = true;
+        }
+    }
+
+    private IEnumerator DetectingAnObject(float value, Fox fox)
+    {
+        yield return new WaitForSeconds(value);
+        fox.Detected();
+    }
+
+    private IEnumerator AlarmOperation()
+    {
+        _source.Play();
+        while (true)
+        {
+            for (float i = 0; i < 100; i++)
+            {
+                _source.volume = 0f + (1f / 100f * i);
+                yield return null;
+            }
+            for (float i = 0; i < 100; i++)
+            {
+                _source.volume = 1f - (1f / 100f * i);
+                yield return null;
+            }
+        }
+    }
+}
